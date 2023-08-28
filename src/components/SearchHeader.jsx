256d1d52041a8a0db9654829a22b251550c15166
kyondoku <kyondoku@gmail.com>
2023-08-29 01:00:45 +0900

    react router 적용하기, header component 생성

---
+++ src/components/SearchHeader.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { Link, Navigate, useNavigate, useParams } from "react-router-dom";
+import { BsYoutube, BsSearch } from "react-icons/bs";
+
+export default function SearchHeader() {
+  const { keyword } = useParams();
+  const [text, setText] = useState("");
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(e);
+    setText("");
+  };
+  useEffect(() => {
+    setText(keyword || "");
+  }, [keyword]);
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/videos/${text}`);
+  };
+
+  return (
+    <header className="w-full flex p-4 text-2xl border-b border-zinc-600 mb-4">
+      <Link to="/" className="flex items-center">
+        <BsYoutube className="text-4xl text-brand" />
+        <h1 className="font-bold ml-2 text-3xl">Youtube</h1>
+      </Link>
+      <form className="w-full flex justify-center" onSubmit={handleSubmit}>
+        <input
+          className="w-7/12 p-2 outline-none bg-black text-gray-50"
+          type="text"
+          placeholder="Search.."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button className="px-4 bg-zinc-600">
+          <BsSearch />
+        </button>
+      </form>
+    </header>
+  );
+}
