9f904f976b45182e5e307ca9b3fcc3ccc6950dcf
kyondoku <kyondoku@gmail.com>
2023-08-30 13:49:50 +0900

    gitignore에 env추가(api key 관리) / api 호출 로직 작성

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,8 +1,45 @@
 import axios from "axios";
 
-export async function search(keyword) {
-  // fetch의 문제점 - json으로 변환필요, 백엔드쪽에서 404, 400과 같은 에러코드들을 성공으로 간주한다(어쨌든 response가 있었으므로)
-  return axios
-    .get(`/videos/${keyword ? "search" : "popular"}.json`)
-    .then((res) => res.data.items);
+export default class Youtube {
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: "https://www.googleapis.com/youtube/v3",
+      params: {
+        key: process.env.REACT_APP_YOUTUBE_API_KEY,
+      },
+    });
+  }
+
+  async search(keyword) {
+    // 함수앞에 #을 붙이면 private함수(class 외부에서 호출불가)
+    return keyword ? this.#searchByKeyword() : this.#mostPopular();
+  }
+
+  async #searchByKeyword(keyword) {
+    // fetch의 문제점 - json으로 변환필요, 백엔드쪽에서 404, 400과 같은 에러코드들을 성공으로 간주한다(어쨌든 response가 있었으므로)
+    return this.httpClient
+      .get("search", {
+        params: {
+          part: "snippet",
+          maxResults: 25,
+          type: "video",
+          q: keyword,
+        },
+      })
+      .then((res) => res.data.items)
+      .then((items) => items.map((item) => ({ ...item, id: item.id.videoId })));
+  }
+
+  async #mostPopular() {
+    // fetch의 문제점 - json으로 변환필요, 백엔드쪽에서 404, 400과 같은 에러코드들을 성공으로 간주한다(어쨌든 response가 있었으므로)
+    return this.httpClient
+      .get("videos", {
+        params: {
+          part: "snippet",
+          maxResults: 25,
+          chart: "mostpopular",
+        },
+      })
+      .then((res) => res.data.items);
+  }
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import React, { useEffect, useState } from "react";
 import { Routes, useParams } from "react-router-dom";
 import VideoCard from "../components/VideoCard";
-import { search } from "../api/youtube";
+import Youtube, { search } from "../api/youtube";
 import FakeYoutube from "../api/fakeYoutube";
 
 export default function Videos() {
@@ -12,7 +12,7 @@ export default function Videos() {
     error,
     data: videos,
   } = useQuery(["videos", keyword], () => {
-    const youtube = new FakeYoutube();
+    const youtube = new Youtube();
     return youtube.search(keyword);
   });
 
